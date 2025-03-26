9ef84d49a96f2f0e58703d8a6cbefecec9747379
Rodrigo Henrique <rhs.buiu@gmail.com>
2025-03-26 18:07:25 -0300

    Atividade da conta atualizada (inseri meus dados no sobre();

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -89,9 +89,9 @@ public class Menu {
 
 	public static void sobre() {
 		System.out.println("\n*********************************************************");
-		System.out.println("Projeto Desenvolvido por: ");
-		System.out.println("Generation Brasil - generation@generation.org");
-		System.out.println("github.com/conteudoGeneration");
+		System.out.println("Projeto Desenvolvido por: Rodrigo Henrique ");
+		System.out.println("Rodrigo Henrique - rodrigos2@genstudents.org");
+		System.out.println("https://github.com/RodrigoHenrikeH");
 		System.out.println("*********************************************************");
 
 	}
