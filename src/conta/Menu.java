1c922b6ec910a8d03b46195e520a5ee12217b2c1
Rodrigo Henrique <rhs.buiu@gmail.com>
2025-04-02 14:57:07 -0300

    PR9- Implementação dos metodos CRUD: atualizar e deletar Conta

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,13 +1,17 @@
 package conta;
 
+
 import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+
+
 
 import conta.controller.ContaController;
 import conta.model.ContaCorrente;
 import conta.model.ContaPoupanca;
 import conta.util.Cores;
+
 
 public class Menu {
 
@@ -120,16 +124,65 @@ public class Menu {
 
 			case 3:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da conta - Por número \n\n");
+				System.out.println("Digite o número da Conta: ");
+				numero = lerScanner.nextInt();
+				
+				contas.procurarPorNumero(numero);
+				
 				keyPress();
 				break;
 
 			case 4:
-				System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da Conta \n\n");
-				keyPress();
+				System.out.println(Cores.TEXT_WHITE + "Atualizar dados da Conta\n\n");
+				
+				System.out.println("Digite o número da conta: ");
+				numero = lerScanner.nextInt();
+				
+				var buscaConta = contas.buscarNaCollection(numero);
+
+				if (buscaConta != null) {
+					
+					System.out.println("Digite o Numero da Agência: ");
+					agencia = lerScanner.nextInt();
+					System.out.println("Digite o Nome do Titular: ");
+					lerScanner.skip("\\R?");
+					titular = lerScanner.nextLine();
+						
+					System.out.println("Digite o Saldo da Conta (R$): ");
+					saldo = lerScanner.nextFloat();
+					
+					tipo = buscaConta.getTipo();
+					
+					switch(tipo) {
+						case 1 -> {
+							System.out.println("Digite o Limite de Crédito (R$): ");
+							limite = lerScanner.nextFloat();
+							contas.atualizar(new ContaCorrente(numero, agencia, tipo, titular, saldo, limite));
+						}
+						case 2 -> {
+							System.out.println("Digite o dia do Aniversario da Conta: ");
+							aniversario = lerScanner.nextInt();
+							contas.atualizar(new ContaPoupanca(numero, agencia, tipo, titular, saldo, aniversario));
+						}
+						default ->{
+							System.out.println("Tipo de conta inválido!");
+						}
+					}
+					
+				}else
+					System.out.println("\nConta não encontrada!");
+
+                keyPress();
 				break;
 
 			case 5:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a Conta \n\n");
+				
+				System.out.println("Digite o número da Conta");
+				numero = lerScanner.nextInt();
+				
+				contas.deletar(numero);
+				
 				keyPress();
 				break;
 
--- src/conta/controller/ContaController.java
+++ src/conta/controller/ContaController.java
@@ -1,26 +1,31 @@
 package conta.controller;
 
 import java.util.ArrayList;
-
 import conta.model.Conta;
 import conta.repository.ContaRepository;
 
 public class ContaController implements ContaRepository {
-	
+
 	private ArrayList<Conta> listaContas = new ArrayList<Conta>();
 	int numero = 0;
 
 	@Override
 	public void procurarPorNumero(int numero) {
+		var conta = buscarNaCollection(numero);
 		
-	}
+		if (conta != null) 
+			conta.visualizar();	
+		else 
+			System.out.println("\nA Conta número: "+ numero +" não foi encontrada!");
+		}
 
+	
 	@Override
 	public void listarTodas() {
 		for (var conta : listaContas) {
 			conta.visualizar();
 		}
-		
+
 	}
 
 	@Override
@@ -28,40 +33,60 @@ public class ContaController implements ContaRepository {
 		listaContas.add(conta);
 		System.out.println("\nA conta Número: " + conta.getNumero() + " Foi criada com Sucesso!");
 	}
-
+	
 	@Override
 	public void atualizar(Conta conta) {
-		// TODO Auto-generated method stub
+		var buscaConta = buscarNaCollection(conta.getNumero());
 		
+		if(buscaConta != null) {
+			listaContas.set(listaContas.indexOf(buscaConta), conta);
+			System.out.println("\nAconta número: " + conta.getNumero() + " foi atualizada com sucesso!");
+		}else 
+			System.out.println("\nA conta numero: " + conta.getNumero() + " não foi encontrada");
+
 	}
 
 	@Override
 	public void deletar(int numero) {
-		// TODO Auto-generated method stub
-		
+		var conta = buscarNaCollection(numero);
+		if(conta != null) {
+			if(listaContas.remove(conta) == true) {
+				System.out.println("\nA conta número " + numero + " foi deletada com sucesso!");
+			}
+		}else
+			System.out.println("\nA conta número " + numero + " não foi encontrada!");
 	}
 
 	@Override
 	public void sacar(int numero, float valor) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void depositar(int numero, float valor) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void Transferir(int numeroOrigem, int numeroDestino, float valor) {
 		// TODO Auto-generated method stub
-		
-	}
-	
-	public int gerarNumero() {
-		return ++ numero;
+
 	}
 
-	
+	public int gerarNumero() {
+		return ++numero;
+	}
+
+	public Conta buscarNaCollection(int numero) {
+		for (var conta : listaContas) {
+			if (conta.getNumero() == numero) {
+				return conta;
+			}
+		}
+		
+		return null;
+	}
+
 }
